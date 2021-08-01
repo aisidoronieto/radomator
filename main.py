40f7421be848e3abb385ff4853fab5b0829992bc
Angel Isidoro Nieto <angel@Angel.local>
2021-08-01 23:19:26 +0100

    main

--- main.py
+++ main.py
@@ -11,69 +11,66 @@ st.text('')
 #st.sidebar.text('')
 #st.sidebar.title('Analisis')
 #st.sidebar.markdown('')
-analysis_type   = st.sidebar.selectbox('Tipo de analisis',['Basico','Enfrentamientos','Rachas','Jugadores','Historico','Plantillas','Otros'],0)
+#analysis_type   = st.sidebar.selectbox('Tipo de analisis',['Basico','Enfrentamientos','Rachas','Jugadores','Historico','Plantillas','Otros'],0)
 #num_players = st.selectbox('Numero de jugadores',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],0)
 
-if num_players == 7:
-    st.title('Jugador 1')
-    player1  = st.text_input("Name",key='1')
-    email1   = st.text_input('Email',key='1')
-    st.title('Jugador 2')
-    player2  = st.text_input("Name",key='2')
-    email2   = st.text_input('Email',key='2')
-    st.title('Jugador 3')
-    player3  = st.text_input("Name",key='3')
-    email3   = st.text_input('Email',key='3')
-    st.title('Jugador 4')
-    player4  = st.text_input("Name",key='4')
-    email4   = st.text_input('Email',key='4')
-    st.title('Jugador 5')
-    player5  = st.text_input("Name",key='5')
-    email5   = st.text_input('Email',key='5')
-    st.title('Jugador 6')
-    player6  = st.text_input("Name",key='6')
-    email6   = st.text_input('Email',key='6')
-    st.title('Jugador 7')
-    player7  = st.text_input("Name",key='7')
-    email7   = st.text_input('Email',key='7')
+#if num_players == 7:
+st.title('Jugador 1')
+player1  = st.text_input("Name",key='1')
+email1   = st.text_input('Email',key='1')
+st.title('Jugador 2')
+player2  = st.text_input("Name",key='2')
+email2   = st.text_input('Email',key='2')
+st.title('Jugador 3')
+player3  = st.text_input("Name",key='3')
+email3   = st.text_input('Email',key='3')
+st.title('Jugador 4')
+player4  = st.text_input("Name",key='4')
+email4   = st.text_input('Email',key='4')
+st.title('Jugador 5')
+player5  = st.text_input("Name",key='5')
+email5   = st.text_input('Email',key='5')
+st.title('Jugador 6')
+player6  = st.text_input("Name",key='6')
+email6   = st.text_input('Email',key='6')
+st.title('Jugador 7')
+player7  = st.text_input("Name",key='7')
+email7   = st.text_input('Email',key='7')
 
 
-    player_names  = [player1,player2,player3,player4,player5,player6,player7]
-    player_emails = [email1,email2,email3,email4,email5,email6,email7]
+player_names  = [player1,player2,player3,player4,player5,player6,player7]
+player_emails = [email1,email2,email3,email4,email5,email6,email7]
 
-    button_value = st.button('Generate')
-  
-    if button_value:
-        df = pd.DataFrame({
-            'playerNames' : player_names,
-            'playerEmails': player_emails,
-        })
+# SEND EMAILS
 
-        df = df.sample(frac=1).reset_index(drop=True)
+button_send = st.button('Enviar correo')
 
-        df
+if button_send:
 
-    # SEND EMAILS
+    df = pd.DataFrame({
+        'playerNames' : player_names,
+        'playerEmails': player_emails,
+    })
 
-    button_send = st.button('Send')
+    df = df.sample(frac=1).reset_index(drop=True)
 
-    if button_send:
-        st.text(df.iloc[1,1])
-
-        # Email 1
-        #yag.send(df.iloc[1,2],'Y tu victima es...', df.iloc[2,1])
-        # Email 1
-        #yag.send('angel.isinie@gmail.com','Y tu victima es...','heyyyyyy')
-        # Email 1
-        #yag.send('angel.isinie@gmail.com','Y tu victima es...','heyyyyyy')
-        # Email 1
-        #yag.send('angel.isinie@gmail.com','Y tu victima es...','heyyyyyy')
-        # Email 1
-        #yag.send('angel.isinie@gmail.com','Y tu victima es...','heyyyyyy')
-        # Email 1
-        #yag.send('angel.isinie@gmail.com','Y tu victima es...','heyyyyyy')
-        # Email 1
-        #yag.send('angel.isinie@gmail.com','Y tu victima es...','heyyyyyy')
+    #df
+    #a=df.iloc[1]['playerNames']
+    #a
+    # Email 1
+    yag.send(df.iloc[0]['playerEmails'],'Y tu victima es...', df.iloc[1]['playerNames'])
+    # Email 1
+    yag.send(df.iloc[1]['playerEmails'],'Y tu victima es...', df.iloc[2]['playerNames'])
+    # Email 1
+    yag.send(df.iloc[2]['playerEmails'],'Y tu victima es...', df.iloc[3]['playerNames'])
+    # Email 1
+    yag.send(df.iloc[3]['playerEmails'],'Y tu victima es...', df.iloc[4]['playerNames'])
+    # Email 1
+    yag.send(df.iloc[4]['playerEmails'],'Y tu victima es...', df.iloc[5]['playerNames'])
+    # Email 1
+    yag.send(df.iloc[5]['playerEmails'],'Y tu victima es...', df.iloc[6]['playerNames'])
+    # Email 1
+    yag.send(df.iloc[6]['playerEmails'],'Y tu victima es...', df.iloc[0]['playerNames'])
 
 
 
